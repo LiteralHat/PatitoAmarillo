abbb1840d7da3e1430a877bfd0df5ae65608a1e8
LiteralHat <literalhatreloaded@gmail.com>
2024-03-19 08:46:40 +1100

    okay

--- elements/sidemenu.php
+++ elements/sidemenu.php
@@ -1,7 +1,12 @@
 
-        <li class='list1'><a href=<?php echo $links . "index.php" ?>>Home</a></li>
-        <li class='list2'><a href=<?php echo $links . "about/about_faq.php" ?>>About</a></li>
-        <ul>
-            <li><a href=<?php echo $links . "about/about_faq.php" ?>>About + FAQ</a></li>
-            <li><a href=<?php echo $links . "about/changelog.php" ?>>Changelog</a></li>
-        </ul>
+
+<li class='list1'><a href=<?php echo $links . "index.php" ?>>Home</a></li>
+<li class='list2'><a href=<?php echo $links . "artworks/gallery.php" ?>>Artworks</a></li>
+<ul>
+    <li><a href=<?php echo $links . "artworks/gallery.php" ?>>Gallery</a></li>
+</ul>
+<li class='list3'><a href=<?php echo $links . "about/about_faq.php" ?>>About</a></li>
+<ul>
+    <li><a href=<?php echo $links . "about/about_faq.php" ?>>About + FAQ</a></li>
+    <li><a href=<?php echo $links . "about/changelog.php" ?>>Changelog</a></li>
+</ul>
