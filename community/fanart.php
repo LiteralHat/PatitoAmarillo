0329c7ac771694ca69f0cb74bc75eb519ccb185a
LiteralHat <literalhatreloaded@gmail.com>
2024-04-05 02:27:35 +1100

    its working as intended

--- community/fanart.php
+++ community/fanart.php
@@ -17,6 +17,13 @@
         <?php include_once ($menutop) ?>
         <!-- side bar on the left.  -->
 
+        <div id='lightBoxElement'>
+            <div id='galleryDisplay'>
+                <h2 class='white'>what the fuck</h2>
+            </div>
+        </div>
+
+
         <div class="contentrowstripe centerbox">
             <div class='boxedsection'>
                 <div class='sidecontainer'>
@@ -39,13 +46,12 @@
                     </div>
 
 
+
                     <div class='contentcontainer'>
                         <div class="whitebox tone3 paddedsm">
                             <div class="whitebox padded">
                                 <h2>Welcome to the fanart gallery.</h2>
-                                <p>This gallery is still a work in progress and I'm mostly using it to test out gallery
-                                    code. More functionality will be coded soon, check the changelog for upcoming features.</p>
-                                    <p class='warning'>Please note that URLs will change in the future; if you decide to hotlink an image from here, it will probably break in the next site release.</p><p>A front page announcement will be made when hotlinking is safe.</p>
+                                <p>
                             </div>
                         </div>
                     </div>
@@ -53,14 +59,26 @@
                     <div class='contentcontainer'>
                         <section class='whitebox toneblack'>
                             <div class='whiteborder padded'>
-                                
-                                    <h2 class='white'>Click to jump to other years:</h2>
-                                    <p class='white'>2024 - 2023 - 2022 - 2021 - 2020</p>
-                                
+
+                                <h2 class='white' id='year'></h2>
+                                <p class='white'>
+                                    <span class='white' onclick='updateYear(2024)'>2024</span>
+                                    -
+                                    <span class='white' onclick='updateYear(2023)'>2023</span>
+                                    -
+                                    <span class='white' onclick='updateYear(2022)'>2022</span>
+                                    -
+                                    <span class='white' onclick='updateYear(2021)'>2021</span>
+                                    -
+                                    <span class='white' onclick='updateYear(2020)'>2020</span>
+                                </p>
+
                                 <hr>
                                 <div class='padtop' id="fanartgallery">
-                                    <!-- jquery appends here -->
+                                    <!-- js appends here -->
                                 </div>
+
+
                             </div>
                         </section>
                     </div>
@@ -82,7 +100,7 @@
         <!-- Footer and closing div tags used for styled main content box  -->
 
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-        <script src="../scripts/galleryfanart.js"></script>
+        <script src="../scripts/fanartgallery.js"></script>
 
 
         <?php include ($footer) ?>
---
+++ scripts/fanartgallery.js
@@ -0,0 +1,62 @@
+const fanartGallery = document.getElementById('fanartgallery');
+const year = document.getElementById('year');
+const lightBoxElement = document.getElementById('lightBoxElement');
+
+
+let galleryList = {
+    2020: ["ari.jpg", "bitterhunibunni-2.jpg", "bitterhunibunni-3.jpg", "bitterhunibunni.jpg", "cactus-2.jpg", "cactus.jpg", "carml_.jpg", "cato.jpg", "cup-1.jpg", "cup-2.jpg", "cup.jpg", "dannie-mango-2.jpg", "dannie-mango-3.jpg", "dannie-mango-4.jpg", "dannie-mango-5.jpg", "dannie-mango-6.jpg", "dannie-mango.jpg", "dannie-mango.jpg", "furumasukusaga-2.jpg", "furumasukusaga.jpg", "hugsnerdsalad-2.jpg", "hugsnerdsalad-3.jpg", "hugsnerdsalad.jpg", "josiexbj.jpg", "julpik-2.jpg", "julpik.jpg", "kimeuro.jpg", "kimeuro400900.jpg", "kira.cant.draw.jpg", "kuudere-potato.jpg", "lakluslime.jpg", "lui-1.jpg", "lui-2.jpg", "lui-3.jpg", "lui-4.jpg", "lui.jpg", "magiciionaboard.jpg", "majolerigolo-2.jpg", "majolerigolo-3.jpg", "majolerigolo-4.jpg", "majolerigolo-5.jpg", "majolerigolo-6.jpg", "majolerigolo-7.jpg", "majolerigolo-8.jpg", "majolerigolo-9.jpg", "majolerigolo.jpg", "mellowdaniel.jpg", "melo-2.jpg", "melo.jpg", "milletsoda.jpg", "mintfresh.jpg", "mug.jpg", "pissbucket-2.jpg", "pissbucket-3.jpg", "pissbucket-4.jpg", "pissbucket-5.jpg", "pissbucket-6.jpg", "pissbucket-7.jpg", "pissbucket-8.jpg", "pissbucket.jpg", "pissbukket.jpg", "princesspoweramazing.jpg", "scissorsmori-10.jpg", "scissorsmori-11.jpg", "scissorsmori-12.jpg", "scissorsmori-13.jpg", "scissorsmori-14.jpg", "scissorsmori-15.jpg", "scissorsmori-2.jpg", "scissorsmori-3.jpg", "scissorsmori-4.jpg", "scissorsmori-5.jpg", "scissorsmori-6.jpg", "scissorsmori-7.jpg", "scissorsmori-8.jpg", "scissorsmori-9.jpg", "scissorsmori_1.jpg", "seth-2.jpg", "seth.jpg", "taliachanx.jpg", "understuckonice-2.jpg", "understuckonice.jpg", "unknown-1.jpg", "unknown-10.jpg", "unknown-11.jpg", "unknown-12.jpg", "unknown-13.jpg", "unknown-14.jpg", "unknown-15.jpg", "unknown-16.jpg", "unknown-17.jpg", "unknown-18.jpg", "unknown-19.jpg", "unknown-2.jpg", "unknown-20.jpg", "unknown-21.jpg", "unknown-22.jpg", "unknown-23.jpg", "unknown-24.jpg", "unknown-25.jpg", "unknown-26.jpg", "unknown-27.jpg", "unknown-28.jpg", "unknown-29.jpg", "unknown-3.jpg", "unknown-30.jpg", "unknown-4.jpg", "unknown-5.jpg", "unknown-6.jpg", "unknown-7.jpg", "unknown-8.jpg", "unknown-9.jpg", "viomee.jpg", "zeagulls-2.jpg", "zeagulls.jpg", "zuma.jpg"],
+    2021: [],
+    2022: [],
+    2023: ["agrumpyrobin.png", "atomizermiser.jpg", "axeman.jpg", "baldintegra.png", "bitterhunnibunni.png", "d3mni.gif", "d3mni.png", "dubstep.png", "furumasuku.png", "hugsnerdsalad.png", "jameng.jpg", "moichein.jpg", "Nolca.png", "paranoidandroid.jpg", "paranoidandroid-2.png", "ramenwaterbottle.jpg", "remi.png", "remi-2.png", "s.v.valan.png", "squid_goes_bork.png", "stupied.idiot.png", "stupied.idiot-2.png", "sugar.png", "tasia.jpg", "unknown.jpg", "unknown.png", "unknown-2.jpg"],
+    2024: []
+};
+
+// Default Values
+let yearLoading = '2023';
+let keyLoading = galleryList[yearLoading].length;
+
+updateYear(yearLoading);
+
+// does the thingy where the pictures show up or whatever lol
+function updateYear(what) {
+    yearLoading = what;
+    keyLoading = galleryList[yearLoading].length;
+    fanartGallery.innerHTML = '';
+    year.innerHTML = `You are currently viewing: ${yearLoading}`
+    for (let z = 0; z <= keyLoading - 1; z++) {
+        let artworkLoading = galleryList[yearLoading][z];
+        let artistName = artworkLoading.replace(/\.[^.]+$/, '');
+        console.log(artistName);
+        fanartGallery.innerHTML += `<img onclick='lightBox("${artistName}")' loading="lazy" src='https://reloaded.literalhat.com/literalfanart/${yearLoading}/literalfanart-${yearLoading}-${artworkLoading}' alt='Fanart by ${artistName}'>`
+    };
+}
+
+function lightBox(name){
+console.log(name);
+};
+
+
+// function checkIfImageExists(url, callback) {
+//     const img = new Image();
+//     img.src = url;
+
+//     if (img.complete) {
+//         callback(true);
+//     } else {
+//         img.onload = () => {
+//             callback(true);
+//         };
+
+//         img.onerror = () => {
+//             callback(false);
+//         };
+//     }
+// }
+
+// checkIfImageExists(`https://reloaded.literalhat.com/literalfanart/${yearLoading}/literalfanart-${yearLoading}-${galleryList[yearLoading][z]}`, (exists) => {
+//     if (exists) {
+//         fanartGallery.innerHTML += `<img loading="lazy" src='https://reloaded.literalhat.com/literalfanart/${yearLoading}/literalfanart-${yearLoading}-${galleryList[yearLoading][z]}'>`
+//     } else {
+//         console.log('rip')
+//     }
+// });
