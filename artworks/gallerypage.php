bcaff0331aa5aa963071986994a903a9d6cb2462
LiteralHat <literalhatreloaded@gmail.com>
2024-04-08 01:33:04 +1000

    gallery mock 2

--- artworks/gallerypage.php
+++ artworks/gallerypage.php
@@ -20,113 +20,86 @@
 
 
         <div class="contentrowstripe centerbox">
-
-            <div class='biggerwidthcontainer'>
-
-                <div class='contentcontainer centerbox'>
-
-                    <img class='galleryimage' src='https://reloaded.literalhat.com/literalhat-2024-bound.png'>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="contentrowblack centerbox">
             <div class='boxedsection'>
-                <div class='biggerwidthcontainer centerbox whitebox'>
-                    <div class='sidecontainer'>
-                        <div id='galleryarrowleftcontainer'>
-                        <div id='galleryarrowleft'></div>
-                        <div id='galleryarrowleftshadow'></div>
-</div>
+                <div class='rowbox'>
+
+                    <div class='contentcontainer'>
+                        <img class='galleryimage' src='https://reloaded.literalhat.com/literalhat-2024-bound.png'>
                     </div>
 
-                    <div class='widthcontainer'>
+                    <div class='thinwidthcontainer'>
                         <div class='contentcontainer'>
-                            <div class="padded">
-                                <div class='spacersmall'></div>
-                                <div class='spacersmall'></div>
-                                <h1 class='large'>ARTWORK TITLE</h1>
-                                <p class='medium'>2024</P>
-                                <p>Anim velit laborum eiusmod magna cillum eiusmod sint dolor ipsum et. Cillum ut
-                                    nostrud
-                                    amet mollit proident. Aute proident non cillum non. Voluptate dolore id occaecat
-                                    exercitation ut elit labore ea.
+                            <div class='whitebox toneblack'>
+                                <div class='whiteborder padded'>
+                                    <div class='spacersmall'></div>
+                                    <div class='spacersmall'></div>
+                                    <h1 class='large white'>ARTWORK TITLE</h1>
+                                    <p class='medium white'>2024</P>
+                                    <p class='white'>Anim velit laborum eiusmod magna cillum eiusmod sint dolor
+                                        ipsum
+                                        et.
+                                        Cillum
+                                        ut
+                                        nostrud
+                                        amet mollit proident. Aute proident non cillum non. Voluptate dolore id
+                                        occaecat
+                                        exercitation ut elit labore ea.
 
-                                </p>
+                                    </p>
 
-                                <hr class='hrtextseparator'>
+                                    <hr>
+                                    <div class='spacersmall'></div>
+                                    <h2 class='white'>Details</h2>
+                                    <dl class='gallerydetails white'>
+                                        <dt>Date</dt>
+                                        <dd>15 October 2023</dd>
 
-                                <h2>Details</h2>
-                                <dl class='gallerydetails'>
-                                    <dt>Date</dt>
-                                    <dd>15 October 2023</dd>
+                                        <dt>Mediums</dt>
+                                        <dd>Acrylic, Gouache</dd>
 
-                                    <dt>Mediums</dt>
-                                    <dd>Acrylic, Gouache</dd>
+                                        <dt>Dimensions</dt>
+                                        <dd></dd>
 
-                                    <dt>Dimensions</dt>
-                                    <dd></dd>
-
-                                    <dt>Share</dt>
-                                    <dd>Insert the logo here</dd>
-                                </dl>
-
-                                <hr class='hrtextseparator'>
-
-                                <h2>Embed</h2>
-                                <div class='whitebox padded'>
-                                    <p></p>
+                                        <dt>Share</dt>
+                                        <dd>Insert the logo here</dd>
+                                    </dl>
+                                    <div class='spacersmall'></div>
+                                    <hr>
+                                    <div class='spacersmall'></div>
+                                    <h2 class='white'>Embed</h2>
+                                    <div class='whitebox toneblack'>
+                                        <div class='whiteborder padded'>
+                                            <p></p>
+                                        </div>
+                                    </div>
                                 </div>
-
                             </div>
                         </div>
-                    </div>
 
+                        <div class='rowbox center'>
+                            <div class='contentcontainer'>
+                                <div class='whitebox paddedsm tone1'>
+                                <hr>
+                                    <h2 class='white padtop'>PREVIOUS ARTWORK</h1>
+                                    <hr>
+                                </div>
+                            </div>
 
-                    <div class='sidecontainer aligndivright'>
-                        <div id='galleryarrowrightcontainer'>
-                            <div id='galleryarrowright'></div>
-                            <div id='galleryarrowrightshadow'></div>
+                            <div class='contentcontainer'>
+                                <div class='whitebox paddedsm tonegreen'>
+                                <hr>
+                                    <h2 class='white padtop'>NEXT ARTWORK</h1>
+                                    <hr>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
+
                 </div>
             </div>
 
-            <div class='spacersmall'></div>
-            <div class='spacersmall'></div>
-
-            <div class='biggerwidthcontainer centerbox'>
-                <div class='whitebox center toneblack'>
-                    <div class='whiteborder padded'>
-                        <h1 class='white'>More artworks</h1>
-                    </div>
-                </div>
-            </div>
-
-            <div class='spacersmall'></div>
         </div>
-        <div class="contentrowwhite centerbox">
-
-
-
-            <div class='spacersmall'></div>
-
-            <div class='biggerwidthcontainer centerbox'>
-
-                <img width=200px src='../images/marla/marla1.jpg'>
-                <img width=200px src='../images/marla/marla1.jpg'>
-                <img width=200px src='../images/marla/marla1.jpg'>
-                <img width=200px src='../images/marla/marla1.jpg'>
-                <img width=200px src='../images/marla/marla1.jpg'>
-
-            </div>
-
-
-        </div>
-
-
 
 
 
