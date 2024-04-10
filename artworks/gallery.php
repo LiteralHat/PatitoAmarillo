7e3a37b4429326bc860ff0732dd54f5b9a6caf08
LiteralHat <literalhatreloaded@gmail.com>
2024-04-11 05:32:59 +1000

    Revert "managed to separate the form code without messing shit up somehow"
    This reverts commit 50a282dcdbf61328abcc7536d742ede5299d8c18.

--- artworks/gallery.php
+++ artworks/gallery.php
@@ -13,6 +13,7 @@
     <main>
 
 
+<<<<<<< HEAD
 
         <div class="contentrowstripe centerbox">
             <div class='boxedsection'>
@@ -73,6 +74,138 @@
                     <div class='spacersmall'>
                     </div>
                 </div>
+=======
+        <?php //code for the header element and load the database for the gallery
+        include_once ($folder . '/elements/galleryheader.php');
+        $db = new PDO('sqlite:artworks.db');
+        $statement = $db->query("SELECT * FROM artworks");
+        //$artworksdb is the 'master' array that will be echo'ed in HTML
+        $artworksdb = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        //code wont execute at all if nothing has been submitted
+        if (isset($_GET['submit'])) {
+
+            function notAllowed($string)
+            {
+                $notallowed = '/[@#$%^&*()+={}\[\]:;<>\/\\|]/';
+                if (preg_match($notallowed, $string)) {
+                    echo "<p class='warning'>Your search query is not valid!</p>";
+                    exit;
+                } else {
+                    return;
+                }
+            }
+
+            // This function compares the master array with the final array and removes the items that they both don't have in common
+            function reSort($artworksdb, $matchingArtworks)
+            {
+                return array_filter($artworksdb, function ($artwork) use ($matchingArtworks) {
+                    return in_array($artwork, $matchingArtworks);
+                });
+            }
+            // This function removes any duplicates from $matchingArtworks using artworkid as the unique identifier
+            function getMatchingArtworks($matchingArtworks, $artworksByTag)
+            {
+                $matchingArtworks = [];
+                foreach ($artworksByTag as $artworks) {
+                    foreach ($artworks as $artwork) {
+                        $matchingArtworks[$artwork['artworkid']] = $artwork;
+                    }
+                }
+                ;
+                return $matchingArtworks;
+            }
+
+            function executeStatement($statement)
+            {
+                $statement->execute();
+                return $statement->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+
+            if (isset($_GET['title'])) {
+                $title = $_GET['title'];
+                notAllowed($title);
+                $queryitem = str_replace(' ', '-', $title);
+                $searchTerm = '%' . $queryitem . '%';
+                $statement = $db->prepare("SELECT * FROM artworks WHERE title LIKE :searchTerm");
+                $statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
+
+                $matchingArtworks = executeStatement($statement);
+                $artworksdb = reSort($artworksdb, $matchingArtworks);
+            }
+
+            if (!empty($_GET['afterdate']) || !empty($_GET['beforedate'])) {
+                if (!empty($_GET['beforedate'])) {
+                    // Use the value from $_GET['beforedate']
+                    $beforedate = $_GET['beforedate'];
+                } else {
+                    // Use a default value if $_GET['beforedate'] is not set
+                    $beforedate = '01-01-1970';
+                }
+
+                if (!empty($_GET['afterdate'])) {
+                    $afterdate = $_GET['afterdate'];
+                } else {
+                    $afterdate = date('Y-m-d');
+                }
+
+                if (isset($_GET['fuzzydate'])) {
+                    $beforedatedt = new DateTime($beforedate);
+                    $afterdatedt = new DateTime($afterdate);
+                    $beforedatedt->modify("-30 days");
+                    $afterdatedt->modify("+30 days");
+                    $beforedate = $beforedatedt->format('Y-m-d');
+                    $afterdate = $afterdatedt->format('Y-m-d');
+                }
+
+                $statement = $db->prepare('SELECT * FROM artworks WHERE datecreated BETWEEN :beforedate AND :afterdate');
+                $statement->bindValue(':beforedate', $beforedate, PDO::PARAM_STR);
+                $statement->bindValue(':afterdate', $afterdate, PDO::PARAM_STR);
+
+                $matchingArtworks = executeStatement($statement);
+                $artworksdb = reSort($artworksdb, $matchingArtworks);
+            }
+
+
+            if (isset($_GET['mediums']) && !empty($_GET['mediums'])) {
+                $answer = $_GET['mediums'];
+                $artworksByTag = [];
+                foreach ($answer as $queryitem) {
+                    $searchTerm = '%' . $queryitem . '%';
+                    $statement = $db->prepare("SELECT * FROM artworks WHERE medium LIKE :searchTerm");
+                    $statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
+
+                    $matchingArtworks = executeStatement($statement);
+
+                    $artworksByTag[$queryitem] = $matchingArtworks;
+                }
+                ;
+                $matchingArtworks = getMatchingArtworks($matchingArtworks, $artworksByTag);
+                $artworksdb = reSort($artworksdb, $matchingArtworks);
+
+            }
+
+            if (isset($_GET['tags'])) {
+                $answer = explode(' ', $_GET['tags']);
+                $artworksByTag = [];
+                foreach ($answer as $queryitem) {
+
+                    $searchTerm = '%' . $queryitem . '%';
+                    $statement = $db->prepare("SELECT * FROM artworks WHERE tags LIKE :searchTerm");
+                    $statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
+
+                    $matchingArtworks = executeStatement($statement);
+                    $artworksByTag[$queryitem] = $matchingArtworks;
+                }
+                ;
+                $matchingArtworks = getMatchingArtworks($matchingArtworks, $artworksByTag);
+                $artworksdb = reSort($artworksdb, $matchingArtworks);
+            }
+
+        }
+        ?>
+>>>>>>> parent of 50a282d (managed to separate the form code without messing shit up somehow)
 
 
                 <div class='sidecontainer'>
@@ -89,10 +222,15 @@
         <div class="contentrowwhite centerbox">
             <div class='boxedsection'>
                 <div class='sidecontainer'>
+<<<<<<< HEAD
 
 
 
                     <form class='form' method='get'>
+=======
+                    <div class='spacermedium'></div>
+                    <form class='form' action='' method='get'>
+>>>>>>> parent of 50a282d (managed to separate the form code without messing shit up somehow)
                         <div class='contentcontainer'>
                             <div class="whitebox padded">
                                 <h2>Advanced Search:</h2>
