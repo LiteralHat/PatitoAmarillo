1f18766e24f1b894a5259f5f553beffe5f9f32de
LiteralHat <literalhatreloaded@gmail.com>
2024-05-11 22:14:49 +1000

    increased efficiency

--- scripts/fanartgallery.js
+++ scripts/fanartgallery.js
@@ -1,23 +1,22 @@
-
 function lightBox(element) {
+    const parent = element.parentElement;
+    const isLightBoxOn = element.classList.contains('lightBoxOn');
 
-    if (element.classList.contains('lightBoxOn')) {
+    if (isLightBoxOn) {
         element.classList.remove('lightBoxOn');
-        let paragraph = element.parentElement.querySelector('h3');
+        const paragraph = parent.querySelector('h3');
         if (paragraph) {
             paragraph.remove();
         }
-
     } else {
-        let artistCredit = element.id.replace(/^[^_]*_/g, "");
-        element.src =  element.src.replace(/leviathan/, 'reloaded');
-        console.log(element.src);
+        const artistCredit = element.id.split('_').slice(1).join('_');
+        element.src = element.src.replace(/leviathan/, 'reloaded');
         element.classList.add('lightBoxOn');
-        let paragraph = document.createElement('h3');
-        paragraph.textContent = 'Fanart By ' + artistCredit ;
-        element.parentElement.appendChild(paragraph);
-
+        const paragraph = document.createElement('h3');
+        paragraph.textContent = 'Fanart By ' + artistCredit;
+        parent.appendChild(paragraph);
     }
-};
+}
+
 
 
