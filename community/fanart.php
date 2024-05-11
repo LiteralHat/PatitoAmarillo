29d92ddfa080ea905239a4947e5eb387c5e1c6c9
LiteralHat <literalhatreloaded@gmail.com>
2024-05-11 21:23:39 +1000

    changed parsing from js to php

--- community/fanart.php
+++ community/fanart.php
@@ -1,4 +1,6 @@
-<?php include_once ('../variables.php') ?>
+<?php include_once ('../variables.php');
+include_once ('../includes/dbh.php');
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,18 +30,15 @@
                 <div class='sidecontainer'>
                     <div class='spacermedium'></div>
                     <?php include_once ($menusimple) ?>
-
                 </div>
 
                 <div class='extrawidthcontainer'>
                     <div class='contentcontainer'>
                         <div class="whitebox center toneblack">
                             <div class='whiteborder padded'>
-                                <hr>
-                                <h1 class='white padtop larger'>
+                                <h1 class='white padtop huge'>
                                     LiteralFanart
                                 </h1>
-                                <hr>
                             </div>
                         </div>
                     </div>
@@ -47,61 +46,44 @@
 
 
                     <div class='contentcontainer'>
-                        <div class="whitebox tone3 paddedsm">
-                            <div class="whitebox padded center">
-                                <h2>Welcome to the fanart gallery.</h2>
-                                <p>This is where you can view artworks made by you.</p>
-                                <p>Click on an image to enlarge and see the artist's name, click again to hide.</p>
-                                <p>For image removals, or if you've been credited with 'unknown'. Email <i>support@literalhat.com</i>.</p>
-                                <p>Please note that links are currently not permanent as I'm still trying to figure out a schema for the site's data. You can hyperlink something if you want to another page, but it may break in the future. </p>
-                                <hr class='hrtextseparator'>
-                                <div class='spacersmall'></div>
-                                <a class='medium center fontheader uppercase nounderline' href='submitfanart.php' target='_blank'>Click here to
-                                        submit your own fanart.</a>
+                        <div class="whitebox toneblack">
+                            <div class="whiteborder paddedsm">
+                                <div class="whitebox padded center">
+                                    <h2>Welcome to the fanart gallery.</h2>
+                                    <p>This is where you can view artworks made by you.</p>
+                                    <p>Click on an image to enlarge and see the artist's name, click again to hide.</p>
+                                    <p>For image removals, or if you've been credited with 'unknown'. Email
+                                        <i>support@literalhat.com</i>.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
 
+
+                    <div class="contentcontainer">
+                        <div class="whitebox tone1 center">
+                            <div class="whiteborder">
+                                <h2 class="padtop white large">SUBMIT YOUR OWN</h2>
+                            </div>
+                        </div>
+                    </div>
+            
                     <div class='contentcontainer'>
                         <section class='whitebox toneblack'>
                             <div class='whiteborder padded'>
-
-                                <!-- <h2 class='white' id='year'></h2>
-                                <p class='white'>
-                                    <span class='white yearhover' onclick='updateYear(2024)'>2024</span>
-                                    -
-                                    <span class='white yearhover' onclick='updateYear(2023)'>2023</span>
-                                    -
-                                    <span class='white yearhover' onclick='updateYear(2022)'>2022</span>
-                                    -
-                                    <span class='white yearhover' onclick='updateYear(2021)'>2021</span>
-                                    -
-                                    <span class='white yearhover' onclick='updateYear(2020)'>2020</span>
-                                </p> -->
-
-                                <hr>
-                                <!-- <div class='center padded' id='loadingDiv'>
-                                    <img src='../images/pagedolls/loading.gif' style='width:300px'>
-                                    <div>
-                                        <span class='fancy large white'>Loading</span>
-                                        <span class='fancy large white' id='loadText'></span>
-                                    </div>
-                                </div> -->
-
-
-                                <!-- <div class='padtop' id="fanartgallery">
-                                </div> -->
-
-
+                                <div class='padtop' id="fanartgallery">
+                                    <?php $statement = $db->query("SELECT * FROM fanart");
+                                    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+                                    foreach ($data as $row => $item) {
+                                        echo "<div><img id='".$item['uuid'] .$item['author'] . "' loading='lazy' class='fanartimage' src='https://leviathan.literalhat.com/fanart/" . $item['uuid'] . "_literalfanart_" . $item['datecreated'] . "_" . $item['author'] . ".webp'></div>";
+                                    }
+                                    ?>
+                                </div>
                             </div>
                         </section>
-
                     </div>
-
-
-
                 </div>
-
 
 
                 <div class='sidecontainer'>
@@ -112,9 +94,9 @@
             </div>
 
         </div>
-        <!-- Footer and closing div tags used for styled main content box  -->
 
-        <!-- <script src="../scripts/fanartgallery.js"></script> -->
+        <!-- Footer and closing div tags used for styled main content box  -->
+        <script src="../scripts/fanartgallery.js"></script>
 
         <?php include ($footer) ?>
     </main>
