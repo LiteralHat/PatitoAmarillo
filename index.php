07c682e2063650082f4dd0971f120654b3f56a28
LiteralHat <literalhatreloaded@gmail.com>
2024-06-21 20:40:23 +1000

    added maintenance page thing

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hi</title>
+</head>
+<body>
+    <h1>Site is being updated!</h1>
+    <p>Yup, right as we speak. This website is being updated. Refresh in a few minutes. For now though, here is a nervous marla:</p>
+    <img src="https://i.imgur.com/UBFpbb4.jpeg">
+</body>
+</html>
